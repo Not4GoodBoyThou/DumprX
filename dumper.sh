5cbb755c4c16b8c3ea04b04534db9d4e57d4a235
Not4GoodBoyThou <78427384+Not4GoodBoyThou@users.noreply.github.com>
2024-06-05 16:40:53 +0530

    Update dumper.sh

--- dumper.sh
+++ dumper.sh
@@ -1110,8 +1110,8 @@ rm -rf "${TMPDIR}" 2>/dev/null
 
 if [[ -s "${PROJECT_DIR}"/.github_token ]]; then
 	GITHUB_TOKEN=$(< "${PROJECT_DIR}"/.github_token)	# Write Your Github Token In a Text File
-	[[ -z "$(git config --get user.email)" ]] && git config user.email "guptasushrut@gmail.com"
-	[[ -z "$(git config --get user.name)" ]] && git config user.name "Sushrut1101"
+	[[ -z "$(git config --get user.email)" ]] && git config user.email "78427384+Not4GoodBoyThou@users.noreply.github.com"
+	[[ -z "$(git config --get user.name)" ]] && git config user.name "Not4GoodBoyThou"
 	if [[ -s "${PROJECT_DIR}"/.github_orgname ]]; then
 		GIT_ORG=$(< "${PROJECT_DIR}"/.github_orgname)	# Set Your Github Organization Name
 	else
